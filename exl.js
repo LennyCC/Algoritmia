799cd9eb0f7b54500f228f5ba84a70f77779a50b
Lenny Cala Casahuaylla <65497879+LennyCC@users.noreply.github.com>
2020-12-28 08:54:52 +0100

    Update exl.js

--- exl.js
+++ exl.js
@@ -30,34 +30,3 @@ const capital = (x) => x.map(i => {
 const capital = (x) => x.map(i => `The Capital of ${i.state ? i.state : i.country} is ${i.capital}`)
 
 
-// LOGS
-const log1 = capital(state_capitals)[0]; // "The capital of Maine is Augusta"
-const log2 = capital(mixed_capitals)[1]; // "The capital of Spain is Madrid"
-const log3 = capital(mixed_capitals)[1]; // "The capital of Spain is Madrid"
-document.getElementById('demo').innerHTML = log3
-
-/* Simple function without conditional */
-// const capital = (x) => x.map(i => `The Capital of ${i.state} is ${i.capital}`);
-
-
-/* Simple function without conditional, no arrow function
-function capital(x) {
-	return x.map(i => `The Capital of ${i.state} is ${i.capital}`)
-}
-
-
-Two ways of calling a specific field of an object:
-
-const myObject = {
-	name: 'Lenny',
-  lastName: 'Cala'
-}
-
-myObject.name
-
-myObject["name"] // porque es útil? imagínate
-
-const x = 'name';
-return myObject.x; // aunque x return 'name', JS lo interpreta literal como que hay un campo que se llama x. Aunque no lo haya. Pero se lo reserva. Detrás del punto LITERAL el nombre del campo. Si tienes que jugar con el nombre usa la segunda forma:
-
-myObject[`${x}`] // Esto si funcionaría, porque sería literalmente "name"*/
